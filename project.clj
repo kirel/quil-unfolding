758cde7daf0bf4d5cee9188b1c88b23734f3589b
Daniel Kirsch <danishkirel@gmail.com>
2015-03-22 14:01:51 +0100

    Add unfolding dependencies to project.clj

--- project.clj
+++ project.clj
@@ -3,5 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-localrepo "0.5.3"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [glgraphics "1.0.0"]
+                 [unfolding "0.9.6"]
+                 [log4j "1.2.17"]
                  [quil "2.2.5"]])
