d773191da015bc2f54d06f068beb1eb852779942
Daniel Kirsch <danishkirel@gmail.com>
2015-03-22 14:41:14 +0100

    Add minimal map visualization

--- src/quil_unfolding/core.clj
+++ src/quil_unfolding/core.clj
@@ -6,34 +6,25 @@
             [quil.middleware :as m]))
 
 (defn setup []
-  ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color 0
-   :angle 0})
+  ; setup function returns initial state.
+  (let [lat 52.5075419 lon 13.4251364 loc (Location. lat lon)]
+    {
+     :lat lat
+     :lon lon
+     :loc loc
+     :map (doto (UnfoldingMap.
+                  (quil.applet/current-applet)
+                  (de.fhpotsdam.unfolding.providers.StamenMapProvider$TonerBackground.))
+            (.zoomToLevel 10)
+            (.panTo loc))
+     }))
 
-(defn update-state [state]
-  ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+(defn update-state [state] state)
 
-(defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
-  (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+(defn draw-state [state] (.draw (state :map)))
 
 (q/defsketch quil-unfolding
   :title "Map"
